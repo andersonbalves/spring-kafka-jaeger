4c8b3b0e2efc575576bc004014b0ed4385e6cc45
andersonbalves <baratella.anderson@gmail.com>
2021-07-02 19:27:54 -0300

    Just checking if git is working properly...

---
+++ src/main/java/br/com/baratella/spring/rest/KafkaController.java
@@ -0,0 +1,36 @@
+package br.com.baratella.spring.rest;
+
+import br.com.baratella.spring.kafka.Producer;
+import br.com.baratella.spring.kafka.avro.User;
+import io.opentracing.Tracer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+@Slf4j
+public class KafkaController {
+
+  private final Producer producer;
+
+  @Autowired
+  private Tracer tracer;
+
+  @Autowired
+  KafkaController(Producer producer) {
+    this.producer = producer;
+  }
+
+  @NewSpan("mySpan")
+  @PostMapping(value = "/publish")
+  public void sendMessageToKafkaTopic(@RequestParam("name") String name, @RequestParam("age") Integer age) {
+    log.debug("tracer: "+tracer);
+    log.info("active span: "+tracer.activeSpan());
+    this.producer.sendMessage(new User(name, age));
+  }
+}
